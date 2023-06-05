ec07c9f19a4ce252266a2a38ae43fc0e260bf9f8
Makskirilyk <122203074+Makskirilyk@users.noreply.github.com>
2023-06-05 20:55:55 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,42 +1,23 @@
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const api_key =
+  'api_key=live_G7nJRuA5s6kytYEBUTv9JbNY1CBU9UkRzIZ72rfs5YZ8bbPez16dz2gS6FCZx5O1';
+
+export function fetchCatByBreed(breedId) {
+  return fetch(`${BASE_URL}images/search?breed_ids=${breedId}&${api_key}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
+
 export function fetchBreeds() {
-    const apiKey = "live_G7nJRuA5s6kytYEBUTv9JbNY1CBU9UkRzIZ72rfs5YZ8bbPez16dz2gS6FCZx5O1"; 
-    return fetch('https://api.thecatapi.com/v1/breeds', {
-      headers: {
-        'x-api-key': apiKey
-      }
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Oops! Something went wrong! Try reloading the page!');
-      }
-      return response.json();
-    })
-    .then(data => {
-      return data;
-    })
-    .catch(error => {
-      throw new Error(error.message);
-    });
-  }
-  
-  export function fetchCatByBreed(breedId) {
-    const apiKey = "live_G7nJRuA5s6kytYEBUTv9JbNY1CBU9UkRzIZ72rfs5YZ8bbPez16dz2gS6FCZx5O1"; 
-  
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, {
-      headers: {
-        'x-api-key': apiKey
-      }
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Oops! Something went wrong! Try reloading the page!');
-      }
-      return response.json();
-    })
-    .then(data => {
-      return data[0];
-    })
-    .catch(error => {
-      throw new Error(error.message);
-    });
-  }
+  return fetch(`${BASE_URL}breeds?${api_key}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
