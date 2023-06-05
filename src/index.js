5f9dcba0aea599062ba3052f2cddd935532f3486
Makskirilyk <122203074+Makskirilyk@users.noreply.github.com>
2023-06-05 20:56:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,65 +1,82 @@
+import './styles.css';
+import Notiflix from 'notiflix';
+import { fetchCatByBreed, fetchBreeds } from './cat-api';
 
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+const breedSelectRef = document.querySelector('.breed-select');
+const catInfoRef = document.querySelector('.cat-info');
+const loaderRef = document.querySelector('.loader');
 
-const breedSelect = document.getElementById('breed-select');
-const catInfoContainer = document.querySelector('.cat-info');
-const catImage = document.getElementById('cat-image');
-const catBreed = document.getElementById('cat-breed');
-const catDescription = document.getElementById('cat-description');
-const catTemperament = document.getElementById('cat-temperament');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
+loaderRef.classList.add('hidden');
+catInfoRef.classList.add('hidden');
+let breedId = '';
 
-// Завантаження списку порід
-function loadBreeds() {
-  loader.style.display = 'block';
-
-  fetchBreeds()
-    .then(breeds => {
-      breeds.forEach(breed => {
-        const option = document.createElement('option');
-        option.value = breed.id;
-        option.textContent = breed.name;
-        breedSelect.appendChild(option);
-      });
-
-      loader.style.display = 'none';
-      breedSelect.style.display = 'block';
-    })
-    .catch(error => {
-      loader.style.display = 'none';
-      error.style.display = 'block';
-    });
-}
-
-function loadCatByBreed(breedId) {
-  loader.style.display = 'block';
-  catInfoContainer.style.display = 'none';
+function showCatByBreed(event) {
+  hideCatInfo();
+  breedId = event.target.value;
 
   fetchCatByBreed(breedId)
-    .then(cat => {
-      catImage.weight = "500px";
-
-      catImage.alt = cat.breeds[0].name;
-      catImage.src = cat.url;
-      catBreed.textContent = cat.breeds[0].name;
-      catDescription.textContent = cat.breeds[0].description;
-      catTemperament.textContent = 'Temperament: ' + cat.breeds[0].temperament;
-
-      loader.style.display = 'none';
-      catInfoContainer.style.display = 'flex';
-    })
+    .then(markupCurrentBreed)
     .catch(error => {
-      loader.style.display = 'none';
-      error.style.display = 'block';
+      loaderRef.classList.add('hidden');
+      errorNotiflix();
     });
 }
 
-breedSelect.addEventListener('change', () => {
-  const selectedBreedId = breedSelect.value;
-  loadCatByBreed(selectedBreedId);
-});
+breedSelectRef.addEventListener('change', showCatByBreed);
 
-document.addEventListener('DOMContentLoaded', () => {
-  loadBreeds();
-});
+function showBreeds() {
+  hideCatInfo();
+
+  fetchBreeds()
+    .then(markupBreedsSelect)
+    .catch(error => {
+      loaderRef.classList.add('hidden');
+      errorNotiflix();
+    });
+}
+
+showBreeds();
+
+function errorNotiflix() {
+  Notiflix.Notify.failure(
+    `Oops! Something went wrong! Try reloading the page!`,
+    {
+      clickToClose: true,
+      timeout: 4000,
+    }
+  );
+}
+
+function markupCurrentBreed(data) {
+  const markup = el => {
+    return ` 
+    <img class="cat-img" src="${el.url}" alt="${el.breeds[0].name}" >
+    <div class="cat-text">
+    <h1 class="cat-header">${el.breeds[0].name}</h1>
+    <p>${el.breeds[0].description}</p>
+    <p><span><b>Temperament: </b></span>${el.breeds[0].temperament}</p>
+    </div>`;
+  };
+
+  catInfoRef.innerHTML = markup(data[0]);
+  loaderRef.classList.add('hidden');
+  catInfoRef.classList.remove('hidden');
+}
+
+function markupBreedsSelect(data) {
+  const markup = data
+    .map(el => {
+      breedId = el.id;
+      return `<option value=${el.id}>${el.name}</option>`;
+    })
+    .join('');
+  breedSelectRef.insertAdjacentHTML('beforeend', markup);
+  loaderRef.classList.add('hidden');
+}
+
+function hideCatInfo() {
+  if (!catInfoRef.classList.contains('hidden')) {
+    catInfoRef.classList.add('hidden');
+  }
+  loaderRef.classList.remove('hidden');
+}
